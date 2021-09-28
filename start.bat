3cbab50b48370dda2e64d79902d820a1004fbeae
Devendra Tiwari <dtiwari18@gmail.com>
2021-09-28 21:55:50 +0530

    Finished

--- start.bat
+++ start.bat
@@ -1,9 +1,7 @@
 @echo off
-set /p reset_required="Reset Required? "
-
-if "%reset_required%"=="y" finish.bat
+if "%1%"=="y" finish.bat
 
 set /p branch_name="Enter branch name :- "
 
 git checkout main
-git checkout -b %branch_name%
+git checkout -b "%branch_name%"
