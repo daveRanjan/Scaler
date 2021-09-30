1bb7dc2ffaa46ae7fbabe8172d657a5dd24cd8e4
Devendra Tiwari <dtiwari18@gmail.com>
2021-09-30 22:27:01 +0530

    Working but not submitted

---
+++ src/main/java/me/learn/arrays/PatternPrinting1.java
@@ -0,0 +1,90 @@
+package me.learn.arrays;
+
+import utils.ArrayUtils;
+
+/**
+ * Pattern Printing -1
+ * Problem Description
+ *
+ * Print a Pattern According to The Given Value of A.
+ *
+ * Example: For A = 3 pattern looks like:
+ *
+ * 1 0 0
+ *
+ * 1 2 0
+ *
+ * 1 2 3
+ *
+ *
+ *
+ * Problem Constraints
+ * 1 <= A <= 1000
+ *
+ *
+ * Input Format
+ * First and only argument is an integer denoting A.
+ *
+ *
+ *
+ * Output Format
+ * Return a two-dimensional array where each row in the returned array represents a row in the pattern.
+ *
+ *
+ *
+ * Example Input
+ * Input 1:
+ *
+ *  A = 3
+ * Input 2:
+ *
+ *  A = 4
+ *
+ *
+ * Example Output
+ * Output :1
+ *
+ *  [
+ *    [1, 0, 0]
+ *    [1, 2, 0]
+ *    [1, 2, 3]
+ *  ]
+ * Output 2:
+ *
+ *  [
+ *    [1, 0, 0, 0]
+ *    [1, 2, 0, 0]
+ *    [1, 2, 3, 0]
+ *    [1, 2, 3, 4]
+ *  ]
+ *
+ *
+ * Example Explanation
+ * Explanation 2:
+ *
+ *
+ *  For A = 4 pattern looks like:
+ *                              1 0 0 0
+ *                              1 2 0 0
+ *                              1 2 3 0
+ *                              1 2 3 4
+ *  So we will return it as two-dimensional array.
+ */
+public class PatternPrinting1 {
+    public static void main(String[] args) {
+        PatternPrinting1 pp1 = new PatternPrinting1();
+        int[][] sol = pp1.solve(4);
+        ArrayUtils.toString(sol);
+    }
+
+    private int[][] solve(int A) {
+        int[][] sol = new int[A][A];
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j <= i; j++) {
+                sol[i][j]=j+1;
+            }
+        }
+
+        return sol;
+    }
+}
